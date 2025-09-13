9176203516c089003ed7570970c2739e8a54c955
Shakhoyat <shakoyatsujon@gmail.com>
2025-09-13 18:39:25 +0600

    Add initial demo project structure for team workflow learning

---
+++ src/app.js
@@ -0,0 +1,32 @@
+// Event Management Ticketing System - Main Application
+// This file will be modified by different teams
+
+class EventManager {
+    constructor() {
+        this.events = [];
+        this.tickets = [];
+    }
+
+    // Team A will work on this feature
+    createEvent(eventData) {
+        console.log("Creating event...");
+        // TODO: Implement event creation logic
+        return null;
+    }
+
+    // Team B will work on this feature
+    bookTicket(eventId, userInfo) {
+        console.log("Booking ticket...");
+        // TODO: Implement ticket booking logic
+        return null;
+    }
+
+    // Team C will work on this feature
+    generateReport() {
+        console.log("Generating report...");
+        // TODO: Implement reporting logic
+        return null;
+    }
+}
+
+module.exports = EventManager;
