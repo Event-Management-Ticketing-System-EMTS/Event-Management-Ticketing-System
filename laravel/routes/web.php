bdb7d3c371cd4b806f7de3e179eb5cfa33d73a75
Shakhoyat <shakoyatsujon@gmail.com>
2025-09-18 04:26:06 +0600

    Add registration routes to web.php

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RegisterController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Registration routes
+Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
+Route::post('/register', [RegisterController::class, 'register'])->name('register.submit');
